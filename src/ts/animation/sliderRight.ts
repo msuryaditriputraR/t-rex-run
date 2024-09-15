599e5558e9bed4ffac196ce10bbf4848d6619006
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-09-16 07:35:16 +0800

    add: animation slider right

---
+++ src/ts/animation/sliderRight.ts
@@ -0,0 +1,14 @@
+export const sliderRight = [
+    {
+        backgroundPosition: "70000%",
+    },
+    {
+        backgroundPosition: "0%",
+    },
+];
+
+export const sliderRightTiming = {
+    duration: 1600000,
+    iterations: Infinity,
+    easing: "linear",
+};
