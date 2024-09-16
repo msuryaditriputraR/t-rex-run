a4c9ecc074b0f2edc020bb10dd3c3444b97ef286
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-09-16 10:17:45 +0800

    add: isJump Context

---
+++ src/js/helper/isJump.js
@@ -0,0 +1,7 @@
+let isJump = false;
+export function getIsJump() {
+    return isJump;
+}
+export function setIsJump(newIsJump) {
+    isJump = newIsJump;
+}
---
+++ src/ts/context/isJump.ts
@@ -0,0 +1,9 @@
+let isJump = false;
+
+export function getIsJump() {
+    return isJump;
+}
+
+export function setIsJump(newIsJump: boolean) {
+    isJump = newIsJump;
+}
