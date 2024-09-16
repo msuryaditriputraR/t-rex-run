544a739d55da6f5dca657c71a37e4baa93a9869a
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-09-16 10:23:56 +0800

    add: control

---
+++ src/js/helper/control.js
@@ -0,0 +1,25 @@
+import { setIsGameRender } from "../context/isGameRender.js";
+export default function control() {
+    const grid = document.querySelector(".grid");
+    const alert = document.querySelector(".alert");
+    const background = document.querySelector(".background");
+    const alertTitle = alert.querySelector(".alert__title");
+    const controlTimerId = setInterval(function () {
+        const obstacle = grid === null || grid === void 0 ? void 0 : grid.querySelector(".obstacle");
+        const dino = grid === null || grid === void 0 ? void 0 : grid.querySelector(".dino");
+        const obstaclePosition = obstacle === null || obstacle === void 0 ? void 0 : obstacle.getBoundingClientRect().x;
+        const dinoPosition = parseFloat(dino === null || dino === void 0 ? void 0 : dino.style.bottom);
+        if (obstaclePosition < 60 && dinoPosition < 60) {
+            clearInterval(controlTimerId);
+            setIsGameRender(false);
+            if (alert && alertTitle) {
+                alert.style.display = "block";
+                alertTitle.textContent = "Game Over";
+            }
+            if (grid) {
+                grid.replaceChildren();
+            }
+            background.style.animationPlayState = "paused";
+        }
+    }, 20);
+}
---
+++ src/ts/helper/control.ts
@@ -0,0 +1,32 @@
+import { setIsGameRender } from "../context/isGameRender.js";
+
+export default function control() {
+    const grid = document.querySelector(".grid");
+    const alert = document.querySelector(".alert") as HTMLElement;
+    const background = document.querySelector(".background") as HTMLElement;
+    const alertTitle = alert.querySelector(".alert__title") as HTMLElement;
+
+    const controlTimerId = setInterval(function () {
+        const obstacle = grid?.querySelector(".obstacle") as HTMLElement;
+        const dino = grid?.querySelector(".dino") as HTMLElement;
+
+        const obstaclePosition = obstacle?.getBoundingClientRect().x;
+        const dinoPosition = parseFloat(dino?.style.bottom);
+
+        if (obstaclePosition < 60 && dinoPosition < 60) {
+            clearInterval(controlTimerId);
+            setIsGameRender(false);
+
+            if (alert && alertTitle) {
+                alert.style.display = "block";
+                alertTitle.textContent = "Game Over";
+            }
+
+            if (grid) {
+                grid.replaceChildren();
+            }
+
+            background.style.animationPlayState = "paused";
+        }
+    }, 20);
+}
