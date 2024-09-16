3147adc2272318846caed5136a3552d3ff795499
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-09-16 10:20:19 +0800

    add: dino

---
+++ src/js/helper/dino.js
@@ -0,0 +1,6 @@
+export default function dino() {
+    const dino = document.createElement("div");
+    dino.classList.add("dino");
+    dino.style.bottom = "5px";
+    return dino;
+}
---
+++ src/ts/helper/dino.ts
@@ -0,0 +1,7 @@
+export default function dino() {
+    const dino = document.createElement("div");
+    dino.classList.add("dino");
+    dino.style.bottom = "5px";
+
+    return dino;
+}
