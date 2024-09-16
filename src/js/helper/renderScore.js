31bf1d9dd29521b7a3a3ca6bf63780dad25bca0c
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-09-16 10:21:17 +0800

    add: renderScore

---
+++ src/js/helper/renderScore.js
@@ -0,0 +1,29 @@
+import { getIsGameRender } from "../context/isGameRender.js";
+import { getScore, setScore } from "../context/score.js";
+export default function renderScore() {
+    const score = document.getElementById("score");
+    const highscore = document.getElementById("highscore");
+    const initialScore = 0;
+    setScore(initialScore);
+    highscore.style.display = "none";
+    const scoreTimerId = setInterval(() => {
+        if (!getIsGameRender()) {
+            if (!localStorage.getItem("highscore")) {
+                localStorage.setItem("highscore", getScore() + "");
+            }
+            else if (Number(localStorage.getItem("highscore")) < getScore()) {
+                localStorage.setItem("highscore", getScore() + "");
+            }
+            highscore.style.display = "initial";
+            highscore.textContent =
+                "HI " +
+                    ("000000" + localStorage.getItem("highscore")).substring((getScore() + "").length);
+            clearInterval(scoreTimerId);
+            return;
+        }
+        setScore(getScore() + 1);
+        if (score) {
+            score.textContent = ("000000" + getScore()).substring((getScore() + "").length);
+        }
+    }, 30);
+}
